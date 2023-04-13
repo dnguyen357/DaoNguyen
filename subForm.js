edc61b979c27426172942a558e8b6d12866e86ac
daonguyen <dn6226@gmail.com>
2023-04-13 18:34:05 -0400

    featureMobile

--- subForm.js
+++ subForm.js
@@ -1,19 +1,19 @@
-const form = document.querySelector("#form")
-const submitButton = document.querySelector("#submit")
-const scriptURL = 'https://script.google.com/macros/s/1XTaVdkdAnzhAHoLOA-x0K4svtJOB9RUnp2OgudFCNoEE4Lo8muDKOgSO/exec'
+const form = document.querySelector("form")
+const submit = document.querySelector("submit")
+const scriptURL = ''
 
 form.addEventListener('submit', e => {
-    submitButton.disabled = true
+    submit.disabled = true
     e.preventDefault()
     let requestBody = new FormData(form)
     fetch(scriptURL, { method: 'POST', body: requestBody})
     .then(response => {
         alert('Success!', response)
-        submitButton.disabled = false
+        submit.disabled = false
         })
     .catch(error => {
     alert('Error!', error.message)
-        submitButton.disabled = false
+        submit.disabled = false
 
     }
     )
